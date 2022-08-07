cb3b2a190eb4b229c3724f33e46c2fbe907f7165
Pashka <51019643+Advancedboy@users.noreply.github.com>
2022-08-07 16:56:27 +0300

    Added calculate functions

---
+++ FindVector/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindVector
+{
+    internal class Program
+    {
+        static int FindVector(int Fx1, int Fy1, int Fx2, int Fy2)
+        {
+            var Fx = Fx2 - Fx1;
+            var Fy = Fy2 - Fy1;
+            return (int)FindHypotenuse(Fx, Fy);
+        }
+        
+        static int FindHypotenuse(int x, int y)
+        {
+            return (int)Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FindVector(2, 3, 7, -9));
+            Console.ReadLine();
+        }
+
+
+    }
+}
